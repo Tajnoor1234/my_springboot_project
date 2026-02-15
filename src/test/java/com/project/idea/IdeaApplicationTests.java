534521a54469538033eca3fa51f64c78dbe0b48b
TAJNOOR SULTANA <sultana2107108@stud.kuet.ac.bd>
2026-02-16 02:39:49 +0600

    Corrected CI to run tests on PR

--- src/test/java/com/project/idea/IdeaApplicationTests.java
+++ src/test/java/com/project/idea/IdeaApplicationTests.java
@@ -2,12 +2,16 @@ package com.project.idea;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)  // disables Spring Security filters in tests
 class IdeaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
