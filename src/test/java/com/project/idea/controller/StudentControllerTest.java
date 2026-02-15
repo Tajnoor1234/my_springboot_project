d6d71b8bb8b54360c492086025dbcf4679c2bb3a
TAJNOOR SULTANA <sultana2107108@stud.kuet.ac.bd>
2026-02-15 23:14:32 +0600

    Initial commit

---
+++ src/test/java/com/project/idea/controller/StudentControllerTest.java
@@ -0,0 +1,29 @@
+package com.project.idea.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void studentDashboard() {
+
+    }
+
+    @Test
+    void addCourseToStudent() {
+    }
+
+    @Test
+    void removeCourseFromStudent() {
+    }
+
+    @Test
+    void showMyCourses() {
+    }
+
+    @Test
+    void showAvailableCourses() {
+    }
+}
